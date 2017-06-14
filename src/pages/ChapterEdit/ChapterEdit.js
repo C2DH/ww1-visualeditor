a8edf3e5c566cb609bbe0b089c45a5898ff033f0
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-14 11:03:46 +0200

    closes #13

--- src/pages/ChapterEdit/ChapterEdit.js
+++ src/pages/ChapterEdit/ChapterEdit.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import SideEditToolbar from '../../components/SideEditToolbar'
+import './ChapterEdit.css'
 
 const ChapterEdit = () => (
-  <div>chapter edit page</div>
+  <Container fluid className="margin-r-l-20">
+    <Row>
+      <Col md="3">
+        <SideEditToolbar />
+      </Col>
+      <Col md="9">
+        <div className="ThemeEdit__right_container" />
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default ChapterEdit
