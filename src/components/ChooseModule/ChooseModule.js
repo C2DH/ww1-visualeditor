44c3d1e94c48e0ea22050a60741ed73b0bed8419
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-09-06 17:49:44 +0200

    choosemodule

--- src/components/ChooseModule/ChooseModule.js
+++ src/components/ChooseModule/ChooseModule.js
@@ -4,28 +4,30 @@ import GenericCard from '../cards/GenericCard'
 import { pure } from 'recompose'
 import './ChooseModule.css'
 
-class ChooseModule extends PureComponent {
-  state = {
-    selected: false
-  }
+const modulesTypes = [
+  {type: 'text', label: 'Module text', cover:'http://via.placeholder.com/100x150'},
+  {type: 'object', label: 'Module object', cover:'http://via.placeholder.com/100x150'},
+  {type: 'gallery', label: 'Module gallery', cover:'http://via.placeholder.com/100x150'},
+  {type: 'map', label: 'Module map', cover:'http://via.placeholder.com/100x150'},
+  {type: 'text_object', label: 'Module text & object', cover:'http://via.placeholder.com/100x150'},
+  {type: 'text_gallery', label: 'Module text & gallery', cover:'http://via.placeholder.com/100x150'},
+  {type: 'text_map', label: 'Module text & map', cover:'http://via.placeholder.com/100x150'},
+]
 
-  handleSelect = () => {
-    this.setState({
-      selected: !this.state.selected
-    })
-  }
+const ChooseModuleCard = pure(({ title, cover, cardClick }) => (
+  <GenericCard
+    className="ChooseModule__ChooseModuleCard"
+    title={title}
+    backgroundImage={cover}
+    cardClick={cardClick}
+  />
+))
+
+
+class ChooseModule extends PureComponent {
 
   render() {
     const { onChooseModule } = this.props
-    const ChooseModuleCard = pure(({ title, cover, cardClick }) => (
-      <GenericCard
-        className="ChooseModule__ChooseModuleCard"
-        title={title}
-        backgroundImage={cover}
-        cardClick={cardClick}
-        selected={this.state.selected}
-      />
-    ))
 
     return (
       <Container fluid className="margin-r-l-20">
@@ -35,41 +37,18 @@ class ChooseModule extends PureComponent {
               <Label for="exampleSelect">Chose a module</Label>
               <div className="Module__action_bottom_btn_container">
                 <hr />
-                <Button size="sm" block>Done</Button>
+                <Button size="sm" block>Next</Button>
                 <Button size="sm" block>Exit</Button>
              </div>
            </div>
           </Col>
           <Col md="9" className="ChooseModule__mods_container">
             <Row>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('text')}>text</button> */}
-                <ChooseModuleCard title="Module text" cardClick={this.handleSelect} cover="http://via.placeholder.com/100x150"/>
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('object')}>object</button> */}
-                <ChooseModuleCard title="Module object" cardClick={() => onChooseModule('object')} cover="http://via.placeholder.com/100x150" />
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('gallery')}>gallery</button> */}
-                <ChooseModuleCard title="Module gallery" cardClick={() => onChooseModule('gallery')} cover="http://via.placeholder.com/100x150" />
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('map')}>map</button> */}
-                <ChooseModuleCard title="Module map" cardClick={() => onChooseModule('map')} cover="http://via.placeholder.com/100x150" />
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('text_object')}>text object</button> */}
-                <ChooseModuleCard title="Module text & object" cardClick={() => onChooseModule('text_object')} cover="http://via.placeholder.com/100x150" />
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('text_gallery')}>text gallery</button> */}
-                <ChooseModuleCard title="Module text & gallery" cardClick={() => onChooseModule('text_gallery')} cover="http://via.placeholder.com/100x150" />
-              </Col>
-              <Col md="4">
-                {/* <button onClick={() => onChooseModule('text_map')}>text map</button> */}
-                <ChooseModuleCard title="Module text & map" cardClick={() => onChooseModule('text_map')} cover="http://via.placeholder.com/100x150" />
-              </Col>
+              {modulesTypes.map((modtype, i) => (
+                <Col md="4" key={i}>
+                  <ChooseModuleCard title={modtype.label} cardClick={() => onChooseModule(modtype.type)} cover={modtype.cover}/>
+                </Col>
+              ))}
             </Row>
           </Col>
         </Row>
